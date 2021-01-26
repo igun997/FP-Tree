e8a9078c1bf6cd3e8420e2693ef8a2f6e728b3b5
Indra Gunanda <indra.gunanda@gmail.com>
2021-01-26 21:19:53 +0700

    Other Library

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pyfpgrowth
 from fpgrowth_py import fpgrowth
 
+
 def convert_toInt(s):
     nilai = 0
     if (s == "anemia defisiensi besi defisiensi besi"):
@@ -13,18 +14,20 @@ def convert_toInt(s):
     else:
         nilai = 3
     return nilai
-data = pd.read_csv("training_data.csv","ISO-8859-1",";")
-headers = pd.read_csv("training_data.csv",index_col=0, nrows=0,delimiter=";")
-cleaner = data.copy()
-cleaner = cleaner[["REG_NO","TEST_NAME","RESULT","REF_RANGE","DESCRIPTION"]]
-print(cleaner)
 
+
+data = pd.read_csv("training_data.csv", "ISO-8859-1", ";")
+headers = pd.read_csv("training_data.csv", index_col=0, nrows=0, delimiter=";")
+cleaner = data.copy()
+cleaner = cleaner[["REG_NO", "TEST_NAME", "RESULT", "REF_RANGE", "DESCRIPTION"]]
+print(cleaner)
 
 filterAll = pd.DataFrame(cleaner)
 
-filterAll["DESCRIPTION"] = filterAll[["REG_NO","DESCRIPTION"]].groupby(['REG_NO'])['DESCRIPTION'].transform(lambda x: ','.join(x))
+filterAll["DESCRIPTION"] = filterAll[["REG_NO", "DESCRIPTION"]].groupby(['REG_NO'])['DESCRIPTION'].transform(
+    lambda x: ','.join(x))
 
-filtered = filterAll[["REG_NO","DESCRIPTION"]].drop_duplicates()
+filtered = filterAll[["REG_NO", "DESCRIPTION"]].drop_duplicates()
 filterData = []
 for index in filtered.index:
     uniqueIt = filtered.loc[index, "DESCRIPTION"].split(",")
@@ -33,4 +36,3 @@ for index in filtered.index:
 freqItemSet, rules = fpgrowth(filterData, minSupRatio=0.5, minConf=0.5)
 print(freqItemSet)
 print(rules)
-
